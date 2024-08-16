ba79076d2f4666e4b44c9dfaa05b19af66a3074f
dkyang2001 <dkyang2001@gmail.com>
2024-08-16 05:37:12 -0400

    ios tag

--- src/App.jsx
+++ src/App.jsx
@@ -1030,6 +1030,7 @@ function App() {
   return (
     <>
       <Helmet>
+        <meta name="viewport" content="minimal-ui" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
       </Helmet>
