bcec10783f2de29d08e5510b45a1a2fda6c009f8
dkyang2001 <dkyang2001@gmail.com>
2024-08-23 23:50:05 -0400

    ios gps test

--- src/App.jsx
+++ src/App.jsx
@@ -197,10 +197,20 @@ function App() {
     }, 1000);
 
     if (window.DeviceOrientationEvent) {
-      const isIOS = !(
-        navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
-        navigator.userAgent.match(/AppleWebKit/)
-      );
+      var isIOS = (function () {
+        var iosQuirkPresent = function () {
+          var audio = new Audio();
+
+          audio.volume = 0.5;
+          return audio.volume === 1; // volume cannot be changed from "1" on iOS 12 and below
+        };
+
+        var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+        var isAppleDevice = navigator.userAgent.includes("Macintosh");
+        var isTouchScreen = navigator.maxTouchPoints >= 1; // true for iOS 13 (and hopefully beyond)
+
+        return isIOS || (isAppleDevice && (isTouchScreen || iosQuirkPresent()));
+      })();
       if (isIOS) {
         window.alert("ios");
         DeviceOrientationEvent.requestPermission()
